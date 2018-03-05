7f234b8faefb3f144a079932022d6353b1886e30
Unknown <olahb@freemail.hu>
2018-03-05 20:29:25 +0100

    Lottó

---
+++ src/lotto/LottoNezetController.java
@@ -0,0 +1,210 @@
+package lotto;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.ResourceBundle;
+import java.util.Set;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author balazs
+ */
+public class LottoNezetController implements Initializable {
+    
+//<editor-fold defaultstate="collapsed" desc="Class Variables">
+    private final int MIN  = 1;
+    private final int MAX = 99;
+    private final String WIN0 = "Sajnos nincs találatod!";
+    private final String WIN1 = "Egyesed van a lottón!";
+    private final String WIN2 = "Kettesed van a lottón!";
+    private final String WIN3 = "Hármasod van a lottón!";
+    private final String WIN4 = "Négyesed van a lottón!";
+    private final String WIN5 = "Ötösöd van a lottón!";
+    
+    private int genNum1;
+    private int genNum2;
+    private int genNum3;
+    private int genNum4;
+    private int genNum5;
+    
+    private int selNum1;
+    private int selNum2;
+    private int selNum3;
+    private int selNum4;
+    private int selNum5;
+//</editor-fold>
+
+//<editor-fold defaultstate="collapsed" desc="FXML import">
+    @FXML
+    private Pane basePane;
+    @FXML
+    private Pane alertPane;
+    @FXML
+    private Label alertText;
+    @FXML
+    private Button alertButton;
+    
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label3;
+    @FXML
+    private Label label4;
+    @FXML
+    private Label label5;
+    @FXML
+    private Label resultLabel;
+        
+    @FXML
+    private TextField input1;
+    @FXML
+    private TextField input2;
+    @FXML
+    private TextField input3;
+    @FXML
+    private TextField input4;
+    @FXML
+    private TextField input5;
+//</editor-fold>
+    
+    @FXML
+    private void handleAlertButton(ActionEvent event)
+        {
+        basePane.setDisable(false);
+        basePane.setOpacity(1);
+        alertPane.setVisible(false);
+        alertText.setText(" ");
+        }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) 
+        {
+        genNum1 = 0;
+        genNum2 = 0;
+        genNum3 = 0;
+        genNum4 = 0;
+        genNum5 = 0;
+
+        genNum1 = getRandandomNumber();
+        genNum2 = getRandandomNumber();
+        genNum3 = getRandandomNumber();
+        genNum4 = getRandandomNumber();
+        genNum5 = getRandandomNumber();
+        
+        calculate();
+        }
+  
+    private void calculate()
+        {
+        //Különbözőség ellenőrzés
+        try {
+            selNum1 = Integer.parseInt(input1.getText());
+            selNum2 = Integer.parseInt(input2.getText());
+            selNum3 = Integer.parseInt(input3.getText());
+            selNum4 = Integer.parseInt(input4.getText());
+            selNum5 = Integer.parseInt(input5.getText());
+            }
+        
+        catch (Exception e) 
+            {
+            alert("Nem jó számot adtál meg!");
+            return; 
+            }
+        
+        Set<Integer> selectedNumbers  = new HashSet<>();
+        selectedNumbers.add(selNum1); 
+        selectedNumbers.add(selNum2);
+        selectedNumbers.add(selNum3);
+        selectedNumbers.add(selNum4);
+        selectedNumbers.add(selNum5);
+        
+        if(selectedNumbers.size() < 5)
+            {
+            alert("A számoknak különbözőnek kell lenniük!");
+            return; 
+            }
+        
+        ArrayList<Integer> userNumbers = new ArrayList<>(selectedNumbers);
+        
+        //Számok 1-99 között vannak e
+        for(Integer number : userNumbers)
+            {
+            if(number < MIN || number > MAX)
+                {
+                alert("Minden számnak 0 és 99 közöt kell lennie!");
+                return; 
+                }
+            }
+        
+        label1.setText("" + genNum1);
+        label2.setText("" + genNum2);
+        label3.setText("" + genNum3);
+        label4.setText("" + genNum4);
+        label5.setText("" + genNum5);
+        
+        resultCheck(userNumbers);
+        
+        return; 
+        }
+    
+    private void alert(String text)
+        {
+        basePane.setDisable(true);
+        basePane.setOpacity(0.3);
+        alertPane.setVisible(true);
+        alertText.setText(text);
+        }
+    
+    private int getRandandomNumber()
+        {
+        int random = (int) (Math.random() * MAX) + MIN;
+        
+        if(random == genNum1 || random == genNum2 || random == genNum3 || random == genNum4 || random == genNum5)
+            return getRandandomNumber();
+        
+        return random;
+        }
+    
+    private void resultCheck(ArrayList<Integer> userNumbers)
+        {
+        int result = 0;
+        
+        for(int i=0;i<userNumbers.size();i++)
+            {   
+            if(userNumbers.get(i) == genNum1 || userNumbers.get(i) == genNum2 || userNumbers.get(i) == genNum3 || userNumbers.get(i) == genNum4 || userNumbers.get(i) == genNum5)
+                result++;
+            }
+        
+        switch(result)
+            {
+            case 0: resultLabel.setText(WIN0);
+                   break;
+            case 1: resultLabel.setText(WIN1);
+                   break;
+            case 2: resultLabel.setText(WIN2);
+                   break;
+            case 3: resultLabel.setText(WIN3);
+                   break;
+            case 4: resultLabel.setText(WIN4);
+                   break;
+            case 5: resultLabel.setText(WIN5);
+                   break;
+            }
+        }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
